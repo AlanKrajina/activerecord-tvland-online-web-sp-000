c7387fad85dd74d5f8342af79360b2fe11a0c67d
Alan Krajina <alankrajina88@gmail.com>
2019-07-02 12:16:45 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,7 +9,7 @@ class Actor < ActiveRecord::Base
   def list_roles
    # self.characters.collect do |char|
       binding.pry
-      
+      self.shows.collect do |char|
       
       
  #     "#{char.name - char.shows}"
